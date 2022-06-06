c14051428a040d298e50dec9afcd4c590c491504
Pavel <fun.love.nuts@gmail.com@>
2022-06-06 14:32:56 +0300

    Added spin animation

--- index.js
+++ index.js
@@ -130,7 +130,73 @@ function animateText(el) {
   });
 }
 
+// Rotate skobki
 
+function animateForIntro({ timing, draw, duration }) { 
+  let start = performance.now();
+
+  requestAnimationFrame(function animateForIntro(time) {
+    let timeFraction = (time - start) / duration;
+    if (timeFraction > 1) timeFraction = 1;
+
+    let progress = timing(1.5, timeFraction);
+
+    draw(progress);
+
+    if (timeFraction < 1) {
+      requestAnimationFrame(animateForIntro);
+    }
+
+  });
+}
+
+
+function back(x = 1.5, timeFraction) {
+  return Math.pow(timeFraction, 2) * ((x + 1) * timeFraction - x)
+}
+
+let icon = document.querySelector('.intro__icon')
+
+function rotate(el) {
+  let to = 360
+  let from = 0
+
+  animateForIntro({
+    duration: 1500,
+    timing: back,
+    draw: function (progress) {
+      let result = (to - from) * progress + from;
+      el.style.transform = `rotate(${result}deg)`
+    }
+  })
+}
+
+setInterval(() => {
+  rotate(icon)
+}, 3000);
+
+
+// Animation for codewars
+
+let codeWars = document.querySelector('#codewars')
+
+function spin(el) {
+  let to = 360
+  let from = 0
+
+  animate({
+    duration: 2000,
+    timing: linear,
+    draw: function (progress) {
+      let result = (to - from) * progress + from;
+      el.style.transform = `rotate(${-result}deg)`
+    }
+  })
+}
+
+setInterval(() => {
+  spin(codeWars)
+}, 2000);
 
 
 
