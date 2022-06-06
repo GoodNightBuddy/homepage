b2575ff805ee516b54c5355d37bbede8dee5befd
Pavel <fun.love.nuts@gmail.com@>
2022-06-06 12:47:20 +0300

    Added typing text animation

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 //Sroll up button
 
+
 let btn = document.getElementsByClassName('scroll-down')[0]
 
 btn.addEventListener('click', e => {
@@ -9,11 +10,21 @@ btn.addEventListener('click', e => {
 })
 
 
-window.addEventListener('scroll', e => {
-  if (window.pageYOffset <= 30 + btn.offsetHeight) {
-    btn.style.top = document.documentElement.clientHeight - window.pageYOffset + 'px'
+window.addEventListener('scroll', showScrollUpButton)
+
+function showScrollUpButton() {
+  if (window.pageYOffset === 0) {
+    btn.style.top = document.documentElement.clientHeight + 5 + 'px'
   }
-})
+  if (window.pageYOffset > 0) {
+    btn.style.top = document.documentElement.clientHeight - window.pageYOffset + 5 + 'px'
+    if (btn.style.top <= document.documentElement.clientHeight - 62 + 'px') {
+      btn.style.top = document.documentElement.clientHeight - 62 + 'px'
+    }
+  }
+}
+showScrollUpButton()
+
 
 // Running blocks
 
@@ -28,9 +39,9 @@ function animateContainers() {
     const scrollHeight = document.documentElement.clientHeight
     const animStart = 4
 
-    if(coords.top > 0 - height/animStart && coords.top < scrollHeight - height/animStart) {
+    if (coords.top > 0 - height / animStart && coords.top < scrollHeight - height / animStart) {
       container.classList.add('swipe')
-    }else {
+    } else {
       container.classList.remove('swipe')
     }
   }
@@ -49,60 +60,91 @@ function getCoords(elem) {
 }
 
 
-// animate running block first
+// animate running block for th first load
 
 function animateContainersOnce(time) {
-    let containers = document.getElementsByClassName('container')
+  let containers = document.getElementsByClassName('container')
 
-    let step = 0
+  let step = 0
 
-    for (let container of containers) {
-      setTimeout(() => {
-        container.classList.add('swipe')
-      }, time + step);
+  for (let container of containers) {
+    setTimeout(() => {
+      container.classList.add('swipe')
+    }, time + step);
 
-      step += 200
-    }
+    step += 200
+  }
 }
 
 animateContainersOnce(700)
 
+// Text typing animation
+
+function animate({ timing, draw, duration }) {  // Vspomogatel'naya funkciya
+
+  let start = performance.now();
+
+  requestAnimationFrame(function animate(time) {
+    // timeFraction изменяется от 0 до 1
+    let timeFraction = (time - start) / duration;
+    if (timeFraction > 1) timeFraction = 1;
+
+    // вычисление текущего состояния анимации
+    let progress = timing(timeFraction);
+
+    draw(progress); // отрисовать её
+
+    if (timeFraction < 1) {
+      requestAnimationFrame(animate);
+    }
+
+  });
+}
+
+function linear(timeFraction) {
+  return timeFraction;
+}
+
+let animateList = document.getElementsByClassName('animate-text')
+
+for (let el of animateList) {
+  animateText(el)
+}
+
+function animateText(el) {
+  let text = el.innerText;
+  let height = el.offsetHeight
+  let width = el.offsetWidth
+  el.style.height = height + 'px'
+  el.style.width = width + 'px'
+  let to = text.length,
+    from = 0;
+
+  animate({
+    duration: 5000,
+    timing: linear,
+    draw: function (progress) {
+      let result = (to - from) * progress + from;
+      el.innerText = text.substr(0, Math.ceil(result))
+    }
+  });
+}
+
+
+
+
+
+
+// function bounce(timeFraction) {
+//   for (let a = 0, b = 1; 1; a += b, b /= 2) {
+//     if (timeFraction >= (7 - 4 * a) / 11) {
+//       return -Math.pow((11 - 6 * a - 11 * timeFraction) / 4, 2) + Math.pow(b, 2)
+//     }
+//   }
+// }
 
 // let scrollHeight = Math.max(
 //   document.body.scrollHeight, document.documentElement.scrollHeight,
 //   document.body.offsetHeight, document.documentElement.offsetHeight,
 //   document.body.clientHeight, document.documentElement.clientHeight
 // );
-
-
-
-
-
-
-
-
-// function animate({timing, draw, duration}) {
-
-//   let start = performance.now();
-
-//   requestAnimationFrame(function animate(time) {
-//     // timeFraction изменяется от 0 до 1
-//     let timeFraction = (time - start) / duration;
-//     if (timeFraction > 1) timeFraction = 1;
-
-//     // вычисление текущего состояния анимации
-//     let progress = timing(timeFraction);
-
-//     draw(progress); // отрисовать её
-
-//     if (timeFraction < 1) {
-//       requestAnimationFrame(animate);
-//     }
-
-//   });
-// }
-
-// animate({
-//   duration: 2000,
-
-// })
